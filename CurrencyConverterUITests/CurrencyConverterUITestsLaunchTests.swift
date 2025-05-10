092097960a861437a92a4c2a94c1829f9f10e19d
anrzej <andrzejfijalo@gmail.com>
2025-05-10 18:26:21 +0200

    added screenshots to ui tests

--- CurrencyConverterUITests/CurrencyConverterUITestsLaunchTests.swift
+++ CurrencyConverterUITests/CurrencyConverterUITestsLaunchTests.swift
@@ -22,16 +22,48 @@ final class CurrencyConverterUITestsLaunchTests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
         app.activate()
+
+        // Screenshot after app launch
+        let launchScreenshot = XCUIScreen.main.screenshot()
+        let launchAttachment = XCTAttachment(screenshot: launchScreenshot)
+        launchAttachment.name = "01 - App Launched"
+        launchAttachment.lifetime = .keepAlways
+        add(launchAttachment)
+
         app.buttons["Process to converter"].tap()
+
+        // Screenshot after tapping "Process to converter"
+        let converterScreen = XCUIScreen.main.screenshot()
+        let converterAttachment = XCTAttachment(screenshot: converterScreen)
+        converterAttachment.name = "02 - Converter Screen"
+        converterAttachment.lifetime = .keepAlways
+        add(converterAttachment)
+
         let arrowUpArrowButton = app.buttons["arrow.up.arrow.down.circle.fill"]
         arrowUpArrowButton.tap()
         app.buttons["USD"].tap()
         app.buttons["GBP"].tap()
+
+        // Screenshot after selecting currencies
+        let currencySelected = XCUIScreen.main.screenshot()
+        let currencyAttachment = XCTAttachment(screenshot: currencySelected)
+        currencyAttachment.name = "03 - Currencies Selected"
+        currencyAttachment.lifetime = .keepAlways
+        add(currencyAttachment)
+
         app.staticTexts["EUR"].tap()
         app.buttons["CZK"].tap()
         app.textFields["0"].tap()
         app.buttons["Convert"].tap()
-        
+
+        // Screenshot after conversion
+        let conversionResult = XCUIScreen.main.screenshot()
+        let conversionAttachment = XCTAttachment(screenshot: conversionResult)
+        conversionAttachment.name = "04 - Conversion Result"
+        conversionAttachment.lifetime = .keepAlways
+        add(conversionAttachment)
+
         arrowUpArrowButton.tap()
     }
+
 }
